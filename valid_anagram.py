3e1c13ab23d90649e43112e3e251d950ea48ddfd
Naol Ketema <naolketema55@gmail.com>
2023-07-13 09:23:33 +0300

    updated code

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,22 +1,8 @@
-# def isAnagram(a: str, b: str) -> bool:
-#     if len(a) == len(b):
-#         for i in range(len(a)):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        x = sorted(s)
+        y = sorted(t)
 
-#             if a[i]not in b :
-#                 return False
-#         return True
-#     else:
-#         return False
-def isAnagram(s: str, t: str) -> bool:
-    x = sorted(s)
-    y = sorted(t)
+        b = x == y
 
-    b = x == y
-
-    return b
-    
-
-a = "aasd"
-b= "asda"
-
-print(isAnagram(a,b))
+        return b
